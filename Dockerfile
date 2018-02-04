6b0dec957d55ef4a02ee66e0d3e26055ed06b5ed
Eric Ho <dho.eric@gmail.com>
2018-02-04 12:29:00 +0800

    Using docker multi-stage build

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,13 @@
-FROM golang:onbuild
+FROM golang:1.9.3-alpine3.6 AS build-env
 
-ENTRYPOINT ["go-wrapper", "run"]
+WORKDIR /go/src/github.com/dhoeric/github-cmd
+
+RUN apk add --no-cache git
+
+COPY . .
+RUN go get -v -d && \
+	go build -o app
+
+FROM alpine:3.6
+COPY --from=build-env /go/src/github.com/dhoeric/github-cmd/app .
+ENTRYPOINT ["/app"]
--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 	"net/http"
-	"context"
-	"fmt"
 	"os"
 )
 
@@ -16,7 +16,6 @@ type RepoInfo struct {
 	ForksCount    int
 	LastUpdatedBy string
 }
-
 
 func main() {
 	// Github Auth
@@ -46,7 +45,7 @@ func main() {
 		}
 
 		pack := &RepoInfo{
-			FullName: *repo.FullName,
+			FullName:   *repo.FullName,
 			ForksCount: *repo.ForksCount,
 			StarsCount: *repo.StargazersCount,
 		}
@@ -85,6 +84,45 @@ func main() {
 		if err != nil {
 			fmt.Printf("Repositories.Create() returned error: %v", err)
 			os.Exit(1)
+		}
+	} else if cmd == "user-list-repo" {
+		fmt.Printf("Here is user-list-repo\n")
+		user := os.Args[2]
+
+		repos, _, err := client.Repositories.List(context, user, &github.RepositoryListOptions{})
+		if err != nil {
+			fmt.Printf("Repositories.List() return error: %v", err)
+			os.Exit(1)
+		}
+
+		for _, repo := range repos {
+			pack := &RepoInfo{
+				FullName:   *repo.FullName,
+				ForksCount: *repo.ForksCount,
+				StarsCount: *repo.StargazersCount,
+			}
+
+			fmt.Printf("%+v\n", pack)
+		}
+
+	} else if cmd == "org-list-repo" {
+		fmt.Printf("Here is org-list-repo\n")
+		user := os.Args[2]
+
+		repos, _, err := client.Repositories.ListByOrg(context, user, &github.RepositoryListByOrgOptions{})
+		if err != nil {
+			fmt.Printf("Repositories.ListByOrg() return error: %v", err)
+			os.Exit(1)
+		}
+
+		for _, repo := range repos {
+			pack := &RepoInfo{
+				FullName:   *repo.FullName,
+				ForksCount: *repo.ForksCount,
+				StarsCount: *repo.StargazersCount,
+			}
+
+			fmt.Printf("%+v\n", pack)
 		}
 
 	} else {
